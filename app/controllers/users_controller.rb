cc82ef48ae680e034803dd06bc8d7eec9828971e
GoneTarou <konnpen@gmail.com>
2026-07-23 23:44:32 +0900

    def createを書いた

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,11 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: 'ユーザー登録が完了しました。'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
