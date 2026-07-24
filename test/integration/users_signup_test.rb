dd696fc9dd6662001a1089fb4b0a237c4c8664e5
GoneTarou <konnpen@gmail.com>
2026-07-24 18:23:49 +0900

    signuptest valid signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid signup" do
+    get new_user_path
+
+    assert_difference("User.count", 1) do
+      post users_path, params: {
+        user: {
+          nickname: "sleepyslug",
+          email_address: "test@example.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+    end
+
+    assert_redirected_to root_path
+  end
 end
